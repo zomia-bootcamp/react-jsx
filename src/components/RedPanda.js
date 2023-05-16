bddc15333ac87ba08fb6b3bfa6ace8cb68c0372c
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-16 10:59:48 +0300

    Create RedPanda.js

---
+++ src/components/RedPanda.js
@@ -0,0 +1,23 @@
+//To help us understand, let's ask chatGPT to comment on this code
+
+import React from 'react';
+
+const redPanda = {
+  src: 'https://upload.wikimedia.org/wikipedia/commons/b/b2/Endangered_Red_Panda.jpg',
+  alt: 'Red Panda',
+  width:  '200px'
+};
+
+function RedPanda(){
+    return (
+      <div>
+        <h1>Cute Red Panda</h1>
+        <img 
+          src={redPanda.src}
+          alt={redPanda.alt}
+          width={redPanda.width} />
+      </div>
+    );
+}
+
+export default RedPanda;
