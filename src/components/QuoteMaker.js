50c5f12074a111c7cf39ff33e666e4d312c9c11d
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-16 10:38:07 +0300

    Create QuoteMaker.js

---
+++ src/components/QuoteMaker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function QuoteMaker() {
+    return (
+      <blockquote>
+        <p>
+         It always seems impossible until it's done.
+        </p>
+        <cite>
+          <a target="_blank"
+            href="https://en.wikipedia.org/wiki/Douglas_Huebler">
+            Douglas Huebler
+          </a>
+        </cite>
+      </blockquote>
+    );
+};
+
+export default QuoteMaker;
