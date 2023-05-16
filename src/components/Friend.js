d9c03154cac9795429949855dedea8e33d3a2d99
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-16 11:36:34 +0300

    Create Friend.js

---
+++ src/components/Friend.js
@@ -0,0 +1,14 @@
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys! Wait Up!",
+    src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
+  }
+];
