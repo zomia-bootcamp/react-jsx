57949039fdbd6584e97c77a3a03da45f380880a0
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-16 11:19:55 +0300

    Update MyQuote.js

--- src/components/MyQuote.js
+++ src/components/MyQuote.js
@@ -1,1 +1,62 @@
+// 1. Define a component to render your quote
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+--- Components Attributes: ---
+
+Start the development server to see the rendered UI in your browser. That way you can see your progress being rendered in the browser window as you work.
+
+- Define an object, above the component, called `quote` with two properties: title and src.
+
+- Assign a string to the title property, something like "My Chosen Quote".
+
+- Assign a valid image URL to the src property, representing an image related to the quote.
+
+- Save the changes to MyQuote.js.
+
+- In the MyQuote component, Wrap the content of your reaturn statment with a <div></div> to insure one outmost elemnt.
+
+- Nest an <h1> element inside of your <div> element.
+
+- Use the value of quote.title as the content of the <h1> element.
+
+- Still inside of the <div></div> tags, make a new line after the <h1></h1> tags.
+
+- On your new line, add an <img /> element.
+
+Your <img /> should have two attributes:
+
+  1. an `src` of quote.src
+  2. an `alt` of quote.title
+
+Save the changes to MyQuote.js.
+
 
