2f10762ee58eade0ff23ec43b24dceeb8fd44094
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-07-03 22:03:59 +0300

    Create div.js

---
+++ src/components/div.js
@@ -0,0 +1,9 @@
+/*
+
+Declare a new variable named `myDiv`. Set `myDiv` equal to a JSX <div> element.
+
+Wrap the <div></div> in parentheses, and use indentation and line breaks like in the examples. 
+
+In between the <div></div> tags, nest an <h1></h1> containing the text Hello world.
+
+*/
