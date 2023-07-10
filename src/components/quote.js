640990a1fa1ebc557e5196ee54213d51a1023dc5
Nilkh <khsrawynilofar@gmail.com>
2023-07-10 15:08:08 -0600

    react-jsx-done

--- src/components/quote.js
+++ src/components/quote.js
@@ -7,6 +7,7 @@ const Quote = () => {
 		{ title: 'Quote 3', src: 'Author 3' },
 		// Add more quotes as needed
 	]
+	const randomVariable = true
 
 	const randomIndex = Math.floor(Math.random() * quotes.length)
 
@@ -15,14 +16,21 @@ const Quote = () => {
 	const handleRefresh = () => {
 		window.location.reload()
 	}
+	if(randomVariable ){
+		return (
+			<div>
+				<h2>{randomQuote.title}</h2>
+				<p>- {randomQuote.src}</p>
+				<button onClick={handleRefresh}>Random Quote</button>
+			</div>
+		)
 
-	return (
-		<div>
-			<h2>{randomQuote.title}</h2>
-			<p>- {randomQuote.src}</p>
-			<button onClick={handleRefresh}>Random Quote</button>
-		</div>
-	)
+	}else{
+		return
+		<p>Today's plan: Stay at home and code.</p>
+	}
+
+	
 }
 
 export default Quote
